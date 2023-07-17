7f329347d7f36557ddc3899bf8eac36031efd074
Eduardo Fleury <eduardopinafleury@gmail.com>
2023-07-17 16:52:49 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 class Program {
 	public static void main(String[] args) {
-		System.out.print("Ola mundo");
+		
+		Position pos = new Position(2, 6);
+		System.out.print(pos);
 	}
 }
