74ce1653ad15ad5fec94704d19a2e8622b7e3350
Eduardo Fleury <eduardopinafleury@gmail.com>
2023-07-20 16:04:00 -0300

    Correction

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,26 +1,31 @@
 package boardgame;
 
 public class Position {
-	private int row;
-	private int column;
-	
+
+	private int	row;
+	private int	column;
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
-	
+
 	public int getRow() {
 		return row;
 	}
+
 	public void setRow(int row) {
 		this.row = row;
 	}
+
 	public int getColumn() {
 		return column;
 	}
+
 	public void setColumn(int column) {
 		this.column = column;
 	}
+
 	public void setValues(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -28,7 +33,6 @@ public class Position {
 
 	@Override
 	public String toString() {
-		return "row: " + row + ", column: " + column;
+		return row + ", " + column;
 	}
-	
 }
