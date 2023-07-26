06b49d45f0d359088e54b373e3d2f2fe78335d4d
Eduardo Fleury <eduardopinafleury@gmail.com>
2023-07-26 08:46:54 -0300

    CheckMate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,9 +57,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.print("Turn: "+chessMatch.getTurn());
-		System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("Check!");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
+				System.out.println("Check!");
+			}
+		}else {
+			System.out.println("CheckMate!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+			
 		}
 	}
 
